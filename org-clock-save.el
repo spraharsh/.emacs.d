5e380dd243d192b162ff90a3ac165e7d8c2405fc
Praharsh Suryadevara <praharsharmm@gmail.com>
2020-12-19 12:40:28 -0600

    more deletions

--- org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-clock-save.el - praharsh-tp at <2020-12-06 Sun 16:51>
-(setq org-clock-stored-history '(("/home/praharsh/Dropbox/todo.org_archive" . 130098) ("/home/praharsh/Dropbox/todo.org_archive" . 130556)))
